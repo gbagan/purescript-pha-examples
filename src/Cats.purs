f51b06bce4508ce03be27f652a14e2762c949a94
Guillaume Bagan <guillaume.bagan@liris.cnrs.fr>
2021-04-22 10:07:40 +0200

    update purescript and spago

--- src/Cats.purs
+++ src/Cats.purs
@@ -27,7 +27,7 @@ update RequestCat = do
     res ← liftAff $ AX.get ResponseFormat.json "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cat"
     let status = case res of
                     Left _  → Failure
-                    Right response → do
+                    Right response →
                         case decodeJson response.body >>= (_ .: "data") >>= (_ .: "image_url") of
                             Left _ → Failure
                             Right url → Success url
